8832716949a385643b84d5a460032fa23dd44ad6
James Elder <james@elder-software.com>
2025-04-07 20:07:12 +0200

    updated bundle id and application id to com.eldersoftware.fittick

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.fit_tick_mobile"
+    namespace = "com.eldersoftware.fittick"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.fit_tick_mobile"
+        applicationId = "com.eldersoftware.fittick"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
